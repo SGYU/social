032deeef2042c8f0e3ab3a3557d54f96345b4475
syu <123@qq.com>
2023-06-12 09:53:25 +0800

    fix

--- social_article/src/main/java/com.syu.article/pojo/Article.java
+++ social_article/src/main/java/com.syu.article/pojo/Article.java
@@ -21,7 +21,6 @@ import java.util.Date;
 @Data
 @ToString
 @NoArgsConstructor
-@Component
 public class Article implements Serializable {
     private static final long serialVersionUID = 1L;
     private String id;//ID
